76536c414a0a5cbbf0d4afda25e2ff10c160c445
Illusion-XIII <illusion-xiii@hotmail.com>
2025-01-26 07:52:30 -0500

    Update cum_in_ass.js
    Added two text options.

--- views/npc/fuck/mc/Texts/female/cum_in_ass.js
+++ views/npc/fuck/mc/Texts/female/cum_in_ass.js
@@ -1,6 +1,8 @@
 setup.actionText ??= {};
 setup.actionText.cum_in_ass = {
     texts: [
-        "With a groan, you cum in %name%'s ass really hard, dumping your cum inside her stretched ass."
+        "With a groan, you cum in %name%'s ass really hard, dumping your cum inside her stretched ass.",
+        "As you thrust your length into %name%'s tight rear end, your climax overwhelms you. You let out a primal growl as your cock throbs, filling her with every last drop of your warm seed.",
+        "Your hot seed shoots into %name%'s ass, coating every inch of her tight sphincter, the powerful surge causing her inner muscles to contract around your retreating length."
     ]
 }
